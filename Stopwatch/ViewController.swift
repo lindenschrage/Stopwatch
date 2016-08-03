71e96a8fb3ec33a4f13640b3b0fb02009c5d9dcb
Linden Schrage <LindenSchrage@lindens-macbook-air.local>
2016-08-03 10:19:54 -0700

    Initial Commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Linden Schrage on 8/3/16.
+//  Copyright © 2016 Linden Schrage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
