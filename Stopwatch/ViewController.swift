fa18cfd59734bcb42e81e4479f3d7afea3c54b5f
Linden Schrage <LindenSchrage@Lindens-MacBook-Air.local>
2016-08-03 11:37:23 -0700

    start and stop

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,25 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBAction func startButton(sender: AnyObject) {
-        stopwatchLabel.text = "start"
-    }
+    
     @IBOutlet weak var stopwatchLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    
+    @IBAction func startTime(sender: AnyObject) {
+        stopwatchLabel.text = "start"
+        alternatebuttons()
+    }
     
     @IBAction func stopButton(sender: AnyObject) {
         stopwatchLabel.text = "stop"
+        alternatebuttons()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        stopButton.hidden = true
+        stopButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func alternatebuttons() {
+        startButton.hidden = !startButton.hidden
+        startButton.enabled = !startButton.enabled
+        stopButton.hidden = !stopButton.hidden
+        stopButton.enabled = !stopButton.enabled
+        
+    
+    }
 
 }
 
